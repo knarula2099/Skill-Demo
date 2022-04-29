585aabe27ae6d57cd4482f1f2f6eff0f6afb7199
knarula2099 <kanarula@ucsd.edu>
2022-04-28 18:35:53 -0700

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SkillDemoTest {
-    public void testStringLength(){
-        assertEquals(10,SkillDemo.stringLength("hello there"))//this test will fail initially.
+    public void testStringLength() {
+        assertEquals(10, SkillDemo.stringLength("hello there"));// this test will fail initially.
     }
 }
